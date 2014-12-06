2d1f699be927e63690b13ad798d9030a1a0d2758
johanafy <johanagf@ucm.es>
2014-12-06 18:12:41 +0100

    Cambios de la BD

--- Duocode.sql
+++ Duocode.sql
@@ -1,32 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 4.2.7.1
--- http://www.phpmyadmin.net
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 23-11-2014 a las 18:14:37
--- Versión del servidor: 5.6.20
--- Versión de PHP: 5.5.15
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Base de datos: `Duocode`
---
-CREATE DATABASE IF NOT EXISTS `Duocode` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `Duocode`;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `candidato`
---
+CREATE DATABASE IF NOT EXISTS `duocode` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `duocode`;
 
 DROP TABLE IF EXISTS `candidato`;
 CREATE TABLE IF NOT EXISTS `candidato` (
@@ -41,23 +22,17 @@ CREATE TABLE IF NOT EXISTS `candidato` (
   `lenguajeDestino` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `ejercicio`
---
+DROP TABLE IF EXISTS `desbloqueadapor`;
+CREATE TABLE IF NOT EXISTS `desbloqueadapor` (
+  `idLeccion` int(20) unsigned NOT NULL,
+  `desbloqueadaPor` int(20) unsigned NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `ejercicio`;
 CREATE TABLE IF NOT EXISTS `ejercicio` (
 `ID` int(20) unsigned NOT NULL,
   `idLeccion` int(20) unsigned NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `enunciado`
---
 
 DROP TABLE IF EXISTS `enunciado`;
 CREATE TABLE IF NOT EXISTS `enunciado` (
@@ -67,12 +42,6 @@ CREATE TABLE IF NOT EXISTS `enunciado` (
   `idEjercicio` int(20) unsigned NOT NULL,
   `lenguaje` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `envio`
---
 
 DROP TABLE IF EXISTS `envio`;
 CREATE TABLE IF NOT EXISTS `envio` (
@@ -86,26 +55,13 @@ CREATE TABLE IF NOT EXISTS `envio` (
   `lenguajeDestino` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `favorito`
---
-
 DROP TABLE IF EXISTS `favorito`;
 CREATE TABLE IF NOT EXISTS `favorito` (
-`ID` int(20) unsigned NOT NULL,
   `idUsuario` int(20) unsigned NOT NULL,
   `idEjercicio` int(20) unsigned NOT NULL,
   `lenguajeOrigen` varchar(20) NOT NULL,
   `lenguajeDestino` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `leccion`
---
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `leccion`;
 CREATE TABLE IF NOT EXISTS `leccion` (
@@ -115,23 +71,10 @@ CREATE TABLE IF NOT EXISTS `leccion` (
   `idTema` int(20) unsigned NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `lenguaje`
---
-
 DROP TABLE IF EXISTS `lenguaje`;
 CREATE TABLE IF NOT EXISTS `lenguaje` (
-`ID` int(20) unsigned NOT NULL,
   `nombre` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tema`
---
 
 DROP TABLE IF EXISTS `tema`;
 CREATE TABLE IF NOT EXISTS `tema` (
@@ -139,12 +82,6 @@ CREATE TABLE IF NOT EXISTS `tema` (
   `titulo` varchar(50) NOT NULL,
   `descripcion` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuario`
---
 
 DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE IF NOT EXISTS `usuario` (
@@ -155,129 +92,99 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `rol` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuariovotacandidato`
---
-
 DROP TABLE IF EXISTS `usuariovotacandidato`;
 CREATE TABLE IF NOT EXISTS `usuariovotacandidato` (
   `idUsuario` int(20) unsigned NOT NULL,
   `idCandidato` int(20) unsigned NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Índices para tablas volcadas
---
 
---
--- Indices de la tabla `candidato`
---
 ALTER TABLE `candidato`
- ADD PRIMARY KEY (`ID`);
+ ADD PRIMARY KEY (`ID`), ADD KEY `gestionadoPor` (`gestionadoPor`), ADD KEY `idUsuario` (`idUsuario`), ADD KEY `idEjercicio` (`idEjercicio`), ADD KEY `lenguajeOrigen` (`lenguajeOrigen`), ADD KEY `lenguajeDestino` (`lenguajeDestino`);
 
---
--- Indices de la tabla `ejercicio`
---
+ALTER TABLE `desbloqueadapor`
+ ADD PRIMARY KEY (`idLeccion`,`desbloqueadaPor`), ADD KEY `desbloqueadaPor` (`desbloqueadaPor`);
+
 ALTER TABLE `ejercicio`
- ADD PRIMARY KEY (`ID`);
+ ADD PRIMARY KEY (`ID`), ADD KEY `idLeccion` (`idLeccion`);
 
---
--- Indices de la tabla `enunciado`
---
 ALTER TABLE `enunciado`
- ADD PRIMARY KEY (`ID`);
+ ADD PRIMARY KEY (`ID`), ADD KEY `idEjercicio` (`idEjercicio`), ADD KEY `lenguaje` (`lenguaje`);
 
---
--- Indices de la tabla `envio`
---
 ALTER TABLE `envio`
- ADD PRIMARY KEY (`ID`);
+ ADD PRIMARY KEY (`ID`), ADD UNIQUE KEY `lenguajeOrigen` (`lenguajeOrigen`), ADD UNIQUE KEY `lenguajeDestino` (`lenguajeDestino`), ADD KEY `idUsuario` (`idUsuario`), ADD KEY `idEjercicio` (`idEjercicio`);
 
---
--- Indices de la tabla `favorito`
---
 ALTER TABLE `favorito`
- ADD PRIMARY KEY (`ID`);
+ ADD PRIMARY KEY (`idUsuario`,`idEjercicio`,`lenguajeOrigen`,`lenguajeDestino`), ADD KEY `idEjercicio` (`idEjercicio`), ADD KEY `lenguajeOrigen` (`lenguajeOrigen`), ADD KEY `lenguajeDestino` (`lenguajeDestino`);
 
---
--- Indices de la tabla `leccion`
---
 ALTER TABLE `leccion`
- ADD PRIMARY KEY (`ID`);
+ ADD PRIMARY KEY (`ID`), ADD KEY `idTema` (`idTema`);
 
---
--- Indices de la tabla `lenguaje`
---
 ALTER TABLE `lenguaje`
- ADD PRIMARY KEY (`ID`);
+ ADD PRIMARY KEY (`nombre`);
 
---
--- Indices de la tabla `tema`
---
 ALTER TABLE `tema`
  ADD PRIMARY KEY (`ID`);
 
---
--- Indices de la tabla `usuario`
---
 ALTER TABLE `usuario`
  ADD PRIMARY KEY (`ID`);
 
---
--- Indices de la tabla `usuariovotacandidato`
---
 ALTER TABLE `usuariovotacandidato`
- ADD PRIMARY KEY (`idUsuario`,`idCandidato`);
+ ADD PRIMARY KEY (`idUsuario`,`idCandidato`), ADD KEY `idCandidato` (`idCandidato`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
 
---
--- AUTO_INCREMENT de la tabla `candidato`
---
 ALTER TABLE `candidato`
 MODIFY `ID` int(20) unsigned NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `ejercicio`
---
 ALTER TABLE `ejercicio`
 MODIFY `ID` int(20) unsigned NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `enunciado`
---
 ALTER TABLE `enunciado`
 MODIFY `ID` int(20) unsigned NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `envio`
---
 ALTER TABLE `envio`
 MODIFY `ID` int(20) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `favorito`
---
-ALTER TABLE `favorito`
-MODIFY `ID` int(20) unsigned NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `leccion`
---
 ALTER TABLE `leccion`
 MODIFY `ID` int(20) unsigned NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `tema`
---
 ALTER TABLE `tema`
 MODIFY `ID` int(20) unsigned NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `lenguaje`
-MODIFY `ID` int(20) unsigned NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `usuario`
---
 ALTER TABLE `usuario`
 MODIFY `ID` int(20) unsigned NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `candidato`
+ADD CONSTRAINT `candidato_ibfk_1` FOREIGN KEY (`gestionadoPor`) REFERENCES `usuario` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `candidato_ibfk_2` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `candidato_ibfk_3` FOREIGN KEY (`idEjercicio`) REFERENCES `ejercicio` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `candidato_ibfk_4` FOREIGN KEY (`lenguajeOrigen`) REFERENCES `lenguaje` (`nombre`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `candidato_ibfk_5` FOREIGN KEY (`lenguajeDestino`) REFERENCES `lenguaje` (`nombre`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `desbloqueadapor`
+ADD CONSTRAINT `desbloqueadapor_ibfk_1` FOREIGN KEY (`idLeccion`) REFERENCES `leccion` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `desbloqueadapor_ibfk_2` FOREIGN KEY (`desbloqueadaPor`) REFERENCES `leccion` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `ejercicio`
+ADD CONSTRAINT `ejercicio_ibfk_1` FOREIGN KEY (`idLeccion`) REFERENCES `leccion` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `enunciado`
+ADD CONSTRAINT `enunciado_ibfk_1` FOREIGN KEY (`idEjercicio`) REFERENCES `ejercicio` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `enunciado_ibfk_2` FOREIGN KEY (`lenguaje`) REFERENCES `lenguaje` (`nombre`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `envio`
+ADD CONSTRAINT `envio_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `envio_ibfk_2` FOREIGN KEY (`idEjercicio`) REFERENCES `ejercicio` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `envio_ibfk_3` FOREIGN KEY (`lenguajeOrigen`) REFERENCES `lenguaje` (`nombre`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `envio_ibfk_4` FOREIGN KEY (`lenguajeDestino`) REFERENCES `lenguaje` (`nombre`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `favorito`
+ADD CONSTRAINT `favorito_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `favorito_ibfk_2` FOREIGN KEY (`idEjercicio`) REFERENCES `ejercicio` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `favorito_ibfk_3` FOREIGN KEY (`lenguajeOrigen`) REFERENCES `lenguaje` (`nombre`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `favorito_ibfk_4` FOREIGN KEY (`lenguajeDestino`) REFERENCES `lenguaje` (`nombre`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `leccion`
+ADD CONSTRAINT `leccion_ibfk_1` FOREIGN KEY (`idTema`) REFERENCES `tema` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `usuariovotacandidato`
+ADD CONSTRAINT `usuariovotacandidato_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `usuariovotacandidato_ibfk_2` FOREIGN KEY (`idCandidato`) REFERENCES `candidato` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ ImagenesAntiguas/Duocode.sql
@@ -0,0 +1,283 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 23-11-2014 a las 18:14:37
+-- Versión del servidor: 5.6.20
+-- Versión de PHP: 5.5.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `Duocode`
+--
+CREATE DATABASE IF NOT EXISTS `Duocode` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `Duocode`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `candidato`
+--
+
+DROP TABLE IF EXISTS `candidato`;
+CREATE TABLE IF NOT EXISTS `candidato` (
+`ID` int(20) unsigned NOT NULL,
+  `codigo` text NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `estado` tinyint(1) NOT NULL DEFAULT '1',
+  `gestionadoPor` int(20) unsigned DEFAULT NULL,
+  `idUsuario` int(20) unsigned NOT NULL,
+  `idEjercicio` int(20) unsigned NOT NULL,
+  `lenguajeOrigen` varchar(20) NOT NULL,
+  `lenguajeDestino` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ejercicio`
+--
+
+DROP TABLE IF EXISTS `ejercicio`;
+CREATE TABLE IF NOT EXISTS `ejercicio` (
+`ID` int(20) unsigned NOT NULL,
+  `idLeccion` int(20) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `enunciado`
+--
+
+DROP TABLE IF EXISTS `enunciado`;
+CREATE TABLE IF NOT EXISTS `enunciado` (
+`ID` int(20) unsigned NOT NULL,
+  `texto` text NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `idEjercicio` int(20) unsigned NOT NULL,
+  `lenguaje` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `envio`
+--
+
+DROP TABLE IF EXISTS `envio`;
+CREATE TABLE IF NOT EXISTS `envio` (
+`ID` int(20) NOT NULL,
+  `codigo` text NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `puntuacion` int(20) unsigned NOT NULL DEFAULT '0',
+  `idUsuario` int(20) unsigned NOT NULL,
+  `idEjercicio` int(20) unsigned NOT NULL,
+  `lenguajeOrigen` varchar(20) NOT NULL,
+  `lenguajeDestino` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `favorito`
+--
+
+DROP TABLE IF EXISTS `favorito`;
+CREATE TABLE IF NOT EXISTS `favorito` (
+`ID` int(20) unsigned NOT NULL,
+  `idUsuario` int(20) unsigned NOT NULL,
+  `idEjercicio` int(20) unsigned NOT NULL,
+  `lenguajeOrigen` varchar(20) NOT NULL,
+  `lenguajeDestino` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `leccion`
+--
+
+DROP TABLE IF EXISTS `leccion`;
+CREATE TABLE IF NOT EXISTS `leccion` (
+`ID` int(20) unsigned NOT NULL,
+  `titulo` varchar(50) NOT NULL,
+  `descripcion` text,
+  `idTema` int(20) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `lenguaje`
+--
+
+DROP TABLE IF EXISTS `lenguaje`;
+CREATE TABLE IF NOT EXISTS `lenguaje` (
+`ID` int(20) unsigned NOT NULL,
+  `nombre` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tema`
+--
+
+DROP TABLE IF EXISTS `tema`;
+CREATE TABLE IF NOT EXISTS `tema` (
+`ID` int(20) unsigned NOT NULL,
+  `titulo` varchar(50) NOT NULL,
+  `descripcion` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE IF NOT EXISTS `usuario` (
+`ID` int(20) unsigned NOT NULL,
+  `nick` varchar(20) NOT NULL,
+  `correo` varchar(20) NOT NULL,
+  `pass` varchar(20) NOT NULL,
+  `rol` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuariovotacandidato`
+--
+
+DROP TABLE IF EXISTS `usuariovotacandidato`;
+CREATE TABLE IF NOT EXISTS `usuariovotacandidato` (
+  `idUsuario` int(20) unsigned NOT NULL,
+  `idCandidato` int(20) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `candidato`
+--
+ALTER TABLE `candidato`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `ejercicio`
+--
+ALTER TABLE `ejercicio`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `enunciado`
+--
+ALTER TABLE `enunciado`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `envio`
+--
+ALTER TABLE `envio`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `favorito`
+--
+ALTER TABLE `favorito`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `leccion`
+--
+ALTER TABLE `leccion`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `lenguaje`
+--
+ALTER TABLE `lenguaje`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `tema`
+--
+ALTER TABLE `tema`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `usuariovotacandidato`
+--
+ALTER TABLE `usuariovotacandidato`
+ ADD PRIMARY KEY (`idUsuario`,`idCandidato`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `candidato`
+--
+ALTER TABLE `candidato`
+MODIFY `ID` int(20) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `ejercicio`
+--
+ALTER TABLE `ejercicio`
+MODIFY `ID` int(20) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `enunciado`
+--
+ALTER TABLE `enunciado`
+MODIFY `ID` int(20) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `envio`
+--
+ALTER TABLE `envio`
+MODIFY `ID` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `favorito`
+--
+ALTER TABLE `favorito`
+MODIFY `ID` int(20) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `leccion`
+--
+ALTER TABLE `leccion`
+MODIFY `ID` int(20) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tema`
+--
+ALTER TABLE `tema`
+MODIFY `ID` int(20) unsigned NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `lenguaje`
+MODIFY `ID` int(20) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+MODIFY `ID` int(20) unsigned NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
